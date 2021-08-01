6592e760b6d78e2223c36e3f70465b8457f2a080
JakeyGilly <jakegillman3@gmail.com>
2021-08-01 17:03:37 +0100

    added valueerror to error.py

--- cogs/error.py
+++ cogs/error.py
@@ -20,7 +20,7 @@ class Error(commands.Cog):
             await ctx.send(embed=discord.Embed(color=0x800000).add_field(name=f'Member Not Found', value=f"{error.args[0]}").set_footer(text=f"FinderBot {info.version}"))
         elif isinstance(error, commands.UserNotFound):
             await ctx.send(embed=discord.Embed(color=0x800000).add_field(name=f'User not found in ban list', value=error.args[0].split("\"")[1]+" was unbanned by a moderator or was not banned").set_footer(text=f"FinderBot {info.version}"))
-        elif isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.BadArgument):
+        elif isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.BadArgument) or isinstance(error, ValueError):
             await ctx.send(embed=discord.Embed(description=error.args[0], color=0x800000).add_field(name=f'Usage for command: {ctx.command}', value=f"`{ctx.prefix}{ctx.command} {ctx.command.signature}`\n<> -> Required\n[] -> Optional").set_footer(text=f"FinderBot {info.version}"))
         elif isinstance(error, commands.MissingPermissions):
             await ctx.send(embed=discord.Embed(color=0x800000).add_field(name=f'Missing permissions', value=f"{error.args[0]}").set_footer(text=f"FinderBot {info.version}"))
